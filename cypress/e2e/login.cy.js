3de79e12c4a888ebdf35516df12873422f8eaba3
Jorge Eduardo Rodrigues <eduardorodrigues23@gmail.com>
2026-06-10 10:34:23 -0300

    refactor: move login credentials to fixture

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,30 @@
 describe('Login', () => {
 
   it('CT-001 - Deve realizar login com sucesso', () => {
+
+    cy.fixture('usuarios').then((usuarios) => {
+
+      const usuario = usuarios.usuarioValido
+
     cy.visit('https://front.serverest.dev/login')
-    cy.get('[data-testid="email"]').type('email_teste@gmail.com')
-    cy.get('[data-testid="senha"]').type('senha_teste')
-    cy.get('[data-testid="entrar"]').click()
-    cy.url().should('eq', 'https://front.serverest.dev/home')
-    cy.contains('Serverest Store').should('be.visible')
+
+    cy.get('[data-testid="email"]')
+    .should('be.visible')
+    .type(usuario.email)
+
+    cy.get('[data-testid="senha"]')
+    .should('be.visible')
+    .type(usuario.senha)
+
+    cy.get('[data-testid="entrar"]')
+    .should('be.visible')
+    .click()
+    
+    cy.url()
+    .should('include', '/home')
+
+    cy.contains('Serverest Store')
+    .should('be.visible')
+   })
   })
 })
