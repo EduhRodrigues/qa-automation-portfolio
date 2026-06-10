af060aaa5ae22c2a6abc48687d1fbbce81801bb8
Jorge Eduardo Rodrigues <eduardorodrigues23@gmail.com>
2026-06-10 12:04:01 -0300

    test: add CT-002 invalid password login scenario

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -27,4 +27,33 @@ describe('Login', () => {
     .should('be.visible')
    })
   })
+
+  it('CT-002 - Deve exibir mensagem de erro ao informar senha inválida', () => {
+
+    cy.fixture('usuarios').then((usuarios) => {
+
+      const usuario = usuarios.usuarioSenhaInvalida
+
+    cy.visit('https://front.serverest.dev/login')
+
+    cy.get('[data-testid="email"]')
+    .should('be.visible')
+    .type(usuario.email)
+
+    cy.get('[data-testid="senha"]')
+    .should('be.visible')
+    .type(usuario.senha)
+
+    cy.get('[data-testid="entrar"]')
+    .should('be.visible')
+    .click()
+
+    cy.url()
+    .should('include', '/login')
+
+    cy.get('.alert')
+    .should('be.visible')
+    .should('contain.text', 'Email e/ou senha inválidos')
+   })
+  })
 })
